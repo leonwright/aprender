7b6d416b4de2dfdce882edd69bcf4e9c5953f28e
wrightleo <wrightleo@jncb.com>
2022-07-20 18:32:33 -0500

    initial application setup

---
+++ apps/conjug8/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="conjug8" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ apps/shell/module-federation.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'shell',
+  remotes: ['conjug8', 'vocab'],
+};
